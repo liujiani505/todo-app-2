6d25136519bc3c61fd3b897ef7687236ba9a2b04
jianiliu <liujiani505@gmail.com>
2023-04-14 13:30:31 -0400

    adding selectors in js

---
+++ app.js
@@ -0,0 +1,8 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// event listeners
+
+// function
