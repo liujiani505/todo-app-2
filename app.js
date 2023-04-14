af406848d6201232743d04c83f8ce2301d90e7af
jianiliu <liujiani505@gmail.com>
2023-04-14 14:07:57 -0400

    more styles

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function addTodo(e){
     // Check trash button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>'
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     // append to todo-list
     todoList.appendChild(todoDiv)
